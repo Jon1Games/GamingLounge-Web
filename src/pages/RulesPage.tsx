d7a1fe8762fc8770a36f6b03d322b6ae08a43833
jonas <jonas.saslona@gmx.de>
2024-09-29 20:14:36 +0200

    rules

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -4,16 +4,89 @@ const RulesPage = () => {
     return (
         <>
             <h1>Regelwerk</h1>
-            <h2>Was zu beachten ist</h2>
-            <p>Um aus unseren Fehlern zu lernen und diesmal die Dinge besser zu machen, gibt es diese Regeln,
-                 sind noch nicht perfekt und werden mit der Zeit ergänzt. Solte es eine Regel geben die zu ungenau 
-                 ist oder fehlt ist es empfohlen, auf <a href='https://discord.gaminglounge.me/'>Discord</a> 
-                 ein Ticket zu eröffnen.
-            </p>
-            <h2>Zuständigkeits bereich</h2>
-                <p>Die folgenden Regeln gelten für das Gesammte GamingLounge Netzwerk, dies beinhaltet unter 
-                     anderem den Discord Server und alle anderen Service im zusammenhang mit dem GamingLounge.me Netzwerk.
-                </p>
+            <ol>
+                <h2><li>Was zu beachten ist</li></h2>
+                        Um aus unseren Fehlern zu lernen und diesmal die Dinge besser zu machen, gibt es diese Regeln,
+                            sind noch nicht perfekt und werden mit der Zeit ergänzt. Solte es eine Regel geben die zu ungenau 
+                            ist oder fehlt ist es empfohlen, auf <a href='https://discord.gaminglounge.me/'>Discord</a> 
+                            ein Ticket zu eröffnen.
+                <h2><li>Zuständigkeits bereich</li></h2>
+                        Die folgenden Regeln gelten für das <strong>Gesammte</strong> GamingLounge Netzwerk, dies beinhaltet 
+                        unter anderem den Discord Server und alle anderen Service im zusammenhang mit dem GamingLounge.me Netzwerk.
+                    <h2><li>Allgemeines Verhalten</li></h2>
+                    <ol>
+                        <li>Die Spieler sind selbst für ihr Verhalten verantwortlich, es gelten die algemeinen Verhaltensrichtlienen
+                             wie, Respektvolles verhalten, keine ernstzunehmenden Beleidiegungen, keine Razismus, kein Spam, 
+                             das einhalten der Kanalthemen usw.</li>
+                        <li>Es solte offensichtlich sein, DDoS Angriffe oder sonstiege angriffe auf den Server sind untersagt und haben Konsequenzen.</li>
+                        <li>NSFW Inhalte sind ausschließlich in den dafür gekenzeichneten Channeln erlaubt.</li>
+                        <li>Keine Nutzung von Stimmenverzerrern oder Soundboards (Discords eigendes Soundbord ausgenommen).</li>
+                    </ol>
+                    <h2><li>Webungen</li></h2>
+                    <ol>
+                        <li>Werbungen sind nicht erlaubt (auch nicht per DM), solte jemand vor haben Werbung zu schalten, 
+                            kann er beim Team auf <a href='https://discord.gaminglounge.me/'>Discord</a> um Erlaubnis bitten.</li>
+                    </ol>
+                    <h2><li>Streaming</li></h2>
+                    <ol>
+                        <li>Rechte auf die eigende stime in z.b Streams auf Twitch oder YT endfallen.</li>
+                    </ol>
+                    <h2><li>Minecraft Server</li></h2>
+                    <ul>
+                        <h3><li>Unfaire Vorteile gegenüber anderen</li></h3>
+                            <ol>
+                                <li>Clientmodifikationen, die es einem ermöglichen durch Wände zu sehen, 
+                                    wie z.B. X-ray oder Freecam, sind nicht gestattet, da sie den Spielspaß anderer verderben.</li>
+                                <li>Hackclients sind algemein verboten, dazu zählen clients die das movement beeinflussen, duppeclients, killaura und co.</li>
+                                <li>Autoclicker sind nicht untersagt, der Spieler wird aber wird nach 16 Minuten automatisch gekickt.</li>
+                            </ol>
+                        <h3><li>Farmen und AFK Regelungen</li></h3>
+                        <p>Um zu vermeiden, dass Resourcen keinen Wert haben und um den Handel zwischen Spielern anzuregen, haben wir uns diesmal entschieden 
+                            bestimmte features zu deaktivieren und bestimmte Farmen zu untersagen.</p>
+                        <ol>
+                                <li>Alle Formen von Dupper sind nicht mehr gestattet, sowohl TNT-, carped-, string-, als auch Sanddupper sind deaktiviert. 
+                                    Dies sicher Resourcen wie, Holz, Cobblestone und auch Erze vor einer Inflation.</li>
+                                <li>Um AFK farmen zu ermöglichen, aber gleichzeitig auch einzudämmen, gibt es nach 16 Minuten einen AFK kick, 
+                                    es ist untersagt diesen zu umgehen, dass automatische farmen unter Anwesenheit ist vollkommen okay und wird auch empfohlen.</li>
+                                <li>Unnötiege Hopper und nicht abstellbare Clocks sind zu vermeiden, 
+                                    solte es zu Problemen kommen wird dementsprechend ein qualifiziertes Mittglied des Teams diese farmen deaktivieren, 
+                                    hierbei wird immer die meist lagende Farm deaktiviert. 
+                                    Das platzieren von Commpostern über Hoppern solte diese lag effizienter machen.</li>
+                            </ol>
+                        <h3><li>Shops & öffentliche Telepads</li></h3>
+                        <p>Im Vergleich zum letzten mal, gibt es für Shops dieses mal keine festen Orte, 
+                            wir wollen dadurch die Ortwahl deregulieren und so vielleicht auch die Kreativität der Leute steigern.</p>
+                        <ol>
+                                <li>Shops dürfen überall auf Claims gebaut werden, jedoch muss ihr Gebäude oder ihr Standort an sich, halbwegs gut aussehen, 
+                                    es ist nicht erwünscht einfach nur ein Holzhaus mit zwei Kisten in eine Ecke zu stellen.</li>
+                                <li>Telepads dürfen nicht als Fallen oder Spieler Farmen verwendet werden. Mit /spawn kommt jeder Spieler aus diesen Fallen raus.</li>
+                                <li>Es wir auch empfohlen den Markt nicht zu unterbieten, dies führt einfach nur zum Leiden der Marktwirtschaft das Team behält sich das 
+                                    Recht ein Preisempfehlungen zu geben Bzw. Ausreißer Shops temporär zu deaktivieren.</li>
+                            </ol>
+                        <h3><li>Griefen und Resourcen farmen</li></h3>
+                        <ol>
+                                <li>Es ist untersagt Spieler zu griefen, ungeclaimte Grundstücke sind hiervon ausgeschlossen, 
+                                    das Team wird keine Resourcen zurückerstaten.</li>
+                                <li>Das farmen von Resourcen ist im FreeBuild untersagt, das minen von Erzen ist okay, aber es wird 
+                                    empfohlen auch hier die jeweiliege Farmwelt zu nutzen um einen sauberen FreeBuild zu hinterlassen.</li>
+                            </ol>                       
+                        <h3><li>Bei Lags und Bugs</li></h3>
+                        <ol>
+                                <li>Solte es mal zu Problemen durch Lags kommen, könnte unter Videobeweis oder auch in seltenden Fällen 
+                                    durch Logs das Team verlorende Resourcen wiedererstatten, sofern die Erklärung plausiebel ist.</li>
+                                <li>Dass ausnutzen von Bugs ist untersagt und wird bestarft, solte es zu Bugs führen, welche Items vernichten, 
+                                    kann das Team mit plausiebler Begründung Items ersetzen.</li>
+                            </ol>                        
+                    </ul>
+                    <h2><li>Umgang mit Regelbrüchen</li></h2>
+                    <ol>
+                        <li>Ein vom Team als Verstoß angesehender Vorfall im Konflikt mit den Regeln,
+                             kann zu einer Strafe wie z.B. einem Mute oder Ban führen, das Team nimmt sich das Recht,
+                              dies selbst zu entscheiden, weitere Infos auf <a href='https://discord.gaminglounge.me/'>Discord</a>.</li>
+                        <li>Das Team selbst, kann solte ein Spieler gegen den algemeinen Frieden des Servers handeln, ihn bannen.</li>
+                        <li>Die Regeln gälten nach Interpretation des Teams, nicht der des Spielers, den Anweisungen des Teams ist folge zu leisten.</li>
+                    </ol>
+            </ol>
         </>
     )
   }
